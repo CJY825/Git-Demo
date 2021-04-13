297ce18a7f6828e912b96d6e3ad7dff7ea645d8c
CJY825 <1466727064@qq.com>
2021-04-13 23:02:07 +0800

    test分支修改 增加hello master

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+
+        System.out.println("hello master");
     }
 }
